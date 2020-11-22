a3c1350e587f54a7f3efc2dfcbb5db23b573032b
Aakash Pal <palakash27@gmail.com>
2020-11-23 04:16:22 +0530

    removed ununsed dependencies

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import CountryDetailPage from "./page/CountryDetailPage";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./page/Home";
 import Header from "./components/Header/Header";
 
